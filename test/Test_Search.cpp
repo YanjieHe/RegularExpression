09a490219b865ead8df70ce409ac2da4de51d8d4
Yanjie He <heyanjie0@outlook.com>
2022-03-10 23:40:01 -0500

    落霞与孤鹜齐飞，秋水共长天一色。

--- test/Test_Search.cpp
+++ test/Test_Search.cpp
@@ -13,13 +13,13 @@ TEST_CASE("Test Searching", "[DFAMatrix]")
 {
     SECTION("Test Searching Chinese")
     {
-        auto e1 = Literal(U"落霞");
-        auto e2 = Literal(U"秋水");
-        auto matrix1 = e1->Compile();
-        auto matrix2 = e2->Compile();
+        auto sunset = Literal(U"落霞");
+        auto autumnWater = Literal(U"秋水");
+        auto matrixSunset = sunset->Compile();
+        auto matrixAutumnWater = autumnWater->Compile();
         u32string s = U"落霞与孤鹜齐飞，秋水共长天一色。";
 
-        REQUIRE(matrix1.Search(s.begin(), s.end()) == s.begin());
-        REQUIRE(matrix2.Search(s.begin(), s.end()) == s.begin() + 8);
+        REQUIRE(matrixSunset.Search(s.begin(), s.end()) == s.begin());
+        REQUIRE(matrixAutumnWater.Search(s.begin(), s.end()) == s.begin() + 8);
     }
 }
